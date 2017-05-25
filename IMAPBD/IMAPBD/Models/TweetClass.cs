c63c244f58885b1f357c3f4b2e2814b3c8f4eb2d
caescobar <cescobarreyna@gmail.com>
2017-05-25 16:59:00 -0500

    sadas
    asdas

--- IMAPBD/IMAPBD/Models/TweetClass.cs
+++ IMAPBD/IMAPBD/Models/TweetClass.cs
@@ -222,17 +222,17 @@ namespace IMAPBD.Models
             task.Properties.Add("empresa","viaje");
             task.Properties.Add("origen","Lima");
 
-            foreach(string act in actividades)
-            {
-                task.Properties.Add("Tipo"+i.ToString(), "tipo"+i.ToString());
-                task.Properties.Add("OrigenAct"+i.ToString(),"orien"+i.ToString() );
-                task.Properties.Add("DestinoAct"+i.ToString(), "destino"+i.ToString());
-                task.Properties.Add("FechaSal"+i.ToString(), "fecsal"+i.ToString());
-                task.Properties.Add("FechaLleg"+i.ToString(), "fecll"+i.ToString());
-                task.Properties.Add("costo" + i.ToString(), i);
-                task.Properties.Add("moneda" + i.ToString(), "moneda"+i.ToString());
+            //foreach(string act in actividades)
+            //{
+            //    task.Properties.Add("Tipo"+i.ToString(), "tipo"+i.ToString());
+            //    task.Properties.Add("OrigenAct"+i.ToString(),"orien"+i.ToString() );
+            //    task.Properties.Add("DestinoAct"+i.ToString(), "destino"+i.ToString());
+            //    task.Properties.Add("FechaSal"+i.ToString(), "fecsal"+i.ToString());
+            //    task.Properties.Add("FechaLleg"+i.ToString(), "fecll"+i.ToString());
+            //    task.Properties.Add("costo" + i.ToString(), i);
+            //    task.Properties.Add("moneda" + i.ToString(), "moneda"+i.ToString());
             
-            }
+            //}
 
                 using (DatastoreTransaction transaction = db.BeginTransaction())
                 {
